383ccba11e2b3176316f68a1dcf3b77af8091e66
Lynlee21 <96206081+Lynlee21@users.noreply.github.com>
2022-05-18 14:35:46 -0500

    Inserted Weather App from CodeSandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="weather-app">
+            <form id="search-form" className="mb-3">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Type a city.."
+                    className="form-control"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="overview">
+              <h1 id="city">New York</h1>
+              <ul>
+                <li>
+                  Last updated: <span id="date">12:00 PM</span>
+                </li>
+                <li id="description"></li>
+              </ul>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <div className="clearfix weather-temperature">
+                  <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                    alt="Sunny"
+                    class="float-left"
+                  />
+                  <strong id="temperature">25</strong>
+                  <span className="units">°C | °F</span>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: <span id="humidity">52</span>%
+                  </li>
+                  <li>
+                    Wind: <span id="wind">12</span> mph
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <small>
+            <a href="https://github.com/Lynlee21/weather-app">
+              Open-source code
+            </a>
+            by Lynlee Anderson
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
