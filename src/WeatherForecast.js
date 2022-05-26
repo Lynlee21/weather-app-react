e34253c78e86cd79a829f603040a220dec2c63b4
Lynlee21 <96206081+Lynlee21@users.noreply.github.com>
2022-05-25 20:28:20 -0500

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,13 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
   return (
     <div className="weather-forecast">
       <div className="row">
         <div className="col">
           <div className="weather-forecast-date">Thursday </div>
-          <WeatherIcon code="01d" />
+          <WeatherIcon code="01d" size={36} />
           <div className="weather-forecast-temperature">
             <span className="weather-forecast-temperature">19°</span>{" "}
             <span className="weather-forecast-temperature-min">10°</span>
